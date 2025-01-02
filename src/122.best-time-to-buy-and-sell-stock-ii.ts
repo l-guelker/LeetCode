bd7e666dd65016c3658963c4194f079be1c0a4e1
Lasse <lasse.g.gh@gmail.com>
2025-01-02 02:46:18 +0100

    solved #122

---
+++ src/122.best-time-to-buy-and-sell-stock-ii.ts
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=122 lang=typescript
+ *
+ * [122] Best Time to Buy and Sell Stock II
+ */
+
+// @lc code=start
+function maxProfit(prices: number[]): number {
+    let max_profit: number = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] > prices[i - 1]) {
+            max_profit += prices[i] - prices[i - 1];
+        }
+    }
+
+    return max_profit;
+};
+// @lc code=end
+
